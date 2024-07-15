03d1df920dd9f474b04e50a015376196d2e89d31
scrapdoll <scrapdoll@archlinux.g>
2024-07-15 15:32:45 +0300

    1

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+
+# Обновление системы
 sudo pacman -Syu
 
 sudo pacman -S --needed base-devel git
 
+# Установка Yay
 git clone https://aur.archlinux.org/yay.git
 
 cd yay
@@ -15,14 +18,14 @@ cd ..
 rm -rf yay
 
 
+#TODO: Добавить установку драйверов Nvidia
 
 
 
 
 
 
-
-
+#Запуск Hyprland
 yay -S hyprland
 
 exec Hyprland
